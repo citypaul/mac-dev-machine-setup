923b4d363f43ee944d216015ff58aab673ee129c
Paul Hammond <paul.hammond@gmail.com>
2021-11-13 17:45:18 +0000

    remove exit commands

--- install.sh
+++ install.sh
@@ -5,14 +5,12 @@ then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     echo "eval $(/opt/homebrew/bin/brew shellenv)" >> ~/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)"
-    exit
 fi
 
 if ! command -v ansible &> /dev/null
 then
     brew install ansible
     ansible-galaxy collection install community.general
-    exit
 fi
 
 ansible-playbook local.yaml
