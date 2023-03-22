4ffc661dafb8b4b12f6b3222fac6c20b3cef538d
Paul Hammond <paul.hammond@gmail.com>
2023-03-22 01:15:07 +0000

    set install script to use zsh, not bash

--- install.sh
+++ install.sh
@@ -1,17 +1,15 @@
-#!/bin/bash
+#!/bin/zsh
 
-if ! command -v brew &> /dev/null
-then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    echo "eval $(/opt/homebrew/bin/brew shellenv)" >> ~/.zprofile
+if ! command -v brew &>/dev/null; then
+    /bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo "eval $(/opt/homebrew/bin/brew shellenv)" >>~/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
-if ! command -v ansible &> /dev/null
-then
+if ! command -v ansible &>/dev/null; then
     brew install ansible
 fi
 
 ansible-galaxy install elliotweiser.osx-command-line-tools
 ansible-galaxy collection install community.general
-ansible-playbook local.yaml -K 
+ansible-playbook local.yaml -K
