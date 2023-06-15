847d96526a81035d71f2a053b78314ee02cf4a50
Paul Hammond <paul.hammond@gmail.com>
2023-06-15 22:00:06 +0100

    improved install script - update ansible and brew & error handling (#30)

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+read -p "Would you like to update brew, ansible and ansible extensions to the latest versions? [y/N]: " update_choice
+
 # Check if Homebrew is installed, and install it if not
 if ! command -v brew &>/dev/null; then
     HOMEBREW_INSTALL_URL="https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh"
@@ -10,6 +12,12 @@ if ! command -v brew &>/dev/null; then
 
     echo "eval $(/opt/homebrew/bin/brew shellenv)" >>~/.bashrc
     eval "$(/opt/homebrew/bin/brew shellenv)"
+elif [[ "$update_choice" =~ ^[Yy]$ ]]; then
+    # If Homebrew is installed, upgrade to the latest version
+    brew update && brew upgrade || {
+        echo "Homebrew upgrade failed"
+        exit 1
+    }
 fi
 
 (
@@ -20,16 +28,22 @@ fi
             echo "Ansible installation failed"
             exit 1
         }
+    elif [[ "$update_choice" =~ ^[Yy]$ ]]; then
+        # If Ansible is installed, upgrade to the latest version
+        brew upgrade ansible || {
+            echo "Ansible upgrade failed"
+            exit 1
+        }
     fi
 
     # Install required Ansible roles and collections
-    ansible-galaxy install elliotweiser.osx-command-line-tools || {
-        echo "Failed to install elliotweiser.osx-command-line-tools role"
+    ansible-galaxy install ${update_choice:+--force} elliotweiser.osx-command-line-tools || {
+        echo "Failed to install or upgrade elliotweiser.osx-command-line-tools role"
         exit 1
     }
 
-    ansible-galaxy collection install community.general || {
-        echo "Failed to install community.general collection"
+    ansible-galaxy collection install ${update_choice:+--force} community.general || {
+        echo "Failed to install or upgrade community.general collection"
         exit 1
     }
 
