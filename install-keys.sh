cf6ff77873d014b080cb8f191720896468ae709e
Paul Hammond <paul.hammond@gmail.com>
2024-09-04 11:36:38 +0100

    nice

--- install-keys.sh
+++ install-keys.sh
@@ -5,7 +5,7 @@ if [ ! -f personal-keys.yaml ]; then
     exit 1
 fi
 
-ansible-playbook personal-keys.yaml --tags "api-keys" -K --ask-vault-pass --verbose || {
+ansible-playbook personal-keys.yaml -K --ask-vault-pass --verbose || {
     echo "Failed to execute Ansible playbook for personal keys"
     exit 1
 }
